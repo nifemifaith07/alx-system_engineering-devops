f700eb00c01ae28b88e7387583d1d8f15003103d
nifemifaith07 <nifemifaith07@gmail.com>
2024-09-02 15:09:18 +0100

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,8 +3,8 @@
 file { '/tmp/school':
   ensure => file,
   path => '/tmp/school',
-  mode => '0744'
-  group => 'www-data'
-  owner => 'www-data'
+  mode => '0744',
+  group => 'www-data',
+  owner => 'www-data',
   content => 'I love Puppet'
 }
