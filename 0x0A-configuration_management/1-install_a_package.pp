030ddcddaccf0da67b4ab2ef00fafb33ab5d6a55
nifemifaith07 <nifemifaith07@gmail.com>
2024-09-03 05:18:34 +0100

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-# Ussing Puppet to install flast from pip3
+# Using Puppet to install flast from pip3
+
+package { 'python3':
+  ensure => installed,
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
